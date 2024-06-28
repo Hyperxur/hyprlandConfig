f5a2bee1421842ce9e1b9324007fea29a78fa17d
Hyperxur <hyperxur@gmail.com>
2024-06-28 19:52:41 +0300

    Update files

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Installing programs..."
-sudo pacman -S hyprland hyprpaper waybar neovim alacritty btop cmus wofi grim slurp
+sudo pacman -S hyprland hyprpaper waybar wofi grim slurp
 
 echo "Installing fonts..."
-sudo pacman -S ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono ttf-jetbrains-mono-nerd
+sudo pacman -S ttf-jetbrains-mono-nerd
 
 echo "Copying settings..."
 cp -r .config ~/
